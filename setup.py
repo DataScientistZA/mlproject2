07ae0c027494322248088888e60a3016ac381168
Zahra <zahraphoto93@gmail.com>
2025-09-30 17:03:38 +1000

    setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+#creating package
+
+from setuptools import find_packages, setup
+
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->list[str]:
+    """this function will return the list of requirements"""
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements= [req.replace('\n','') for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+setup(
+    name ="mlproject",
+    version = '0.0.1', 
+    author= 'Zahra',
+    author_email='zahraphoto93@gmail.com',
+    packages=find_packages(),
+    install_requires= get_requirements('requirements.txt')
+)
